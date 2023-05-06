04ca87b9d690690db3fec4b65f0477f17b42e6c7
Andrew <andriyilko@gmail.com>
2023-05-07 02:56:01 +0300

    Update: skeleton block

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d797a0650f77f81f"; }
+/******/ 		__webpack_require__.h = function() { return "03de9d81dbd0fe20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
